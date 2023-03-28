6bf4c4638153415f03ece1b9e92eced19a05f135
djfuylk <129130539+djfuylk@users.noreply.github.com>
2023-03-28 01:51:48 +0000

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=8a6ad5f4-cdb5-4d0f-9191-ad67edeadcf0
+export UUID=22257b9e-237e-41ba-9c65-6ba00dccd469
 
 #请修改为自己设置的伪装站，不要带https://
-boredgamecompany.com
+export ProxySite=arsenalfclatestnews.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
